7dea73581e8a140d884ab1757312bffaa9c51e8b
Vladislav Chernikov <vlad.chernikov.2002@bk.ru>
2023-11-21 22:48:46 +0200

    Исправил проблему с border-bottom у левого меню.

--- views/layout.php
+++ views/layout.php
@@ -20,8 +20,8 @@
             </a>
             <nav class="menu">
                 <ul>
-                    <li class="menu-item"><a href="/" class="menu-link" title="Главная"><span class="span-menu">Главная</span></a></li>
-                    <li class="menu-item"><a href="favorite.php" class="menu-link" title="Избранное"><span class="span-menu">Избранное</span></a></li>
+<!--                    <li class="menu-item"><a href="/" class="menu-link" title="Главная"><span class="span-menu">Главная</span></a></li>-->
+<!--                    <li class="menu-item"><a href="favorite.php" class="menu-link" title="Избранное"><span class="span-menu">Избранное</span></a></li>-->
 					<?=view('pages/sidebar', ['menuItems' => $sideBarMenu]);?>
                 </ul>
             </nav>
